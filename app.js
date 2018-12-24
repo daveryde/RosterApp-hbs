2dd826c545b1debbb107cfcceb67bec930edc801
David Cordova <d.mcordova87@gmail.com>
2018-12-23 18:18:33 -0700

    create database connection and routes

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 
 // Initial express
 const app = express();
 
-// Load routes
-const results = require('./routes/results');
-const people = require('./routes/people');
-
-// Handlebars middleware
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Load models
+require('./models/Person');
+
+// Load routes
+const results = require('./routes/results');
+const people = require('./routes/people');
+
+// Load database keys
+const db = require('./config/keys').mongoURI;
+
+// Connect to the database
+mongoose
+  .connect(
+    db,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+// Load Handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {
+    title: 'Roster App'
+  });
 });
 
 app.use('/results', results);
